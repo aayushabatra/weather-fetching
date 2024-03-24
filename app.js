b3b335d88c18935fdd1b11f540e2471142282814
aayushabatra <147073927+aayushabatra@users.noreply.github.com>
2024-03-24 17:09:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// const apiKey= "4f49037e6f540dadd9b070a4c865a93b";
+// const apiURL= "https://api.openweathermap.org/data/2.5/weather?q&units=metric&q=banglore";
+
+// async function checkWeather(){
+//     const resppnse = await fetch( apiURL + `&appid=${apiKey}`);
+//     var data= await Response.json();
+
+//     console.log(data);
+// }
+
+// checkWeather();
